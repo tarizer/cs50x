2c3cb9bf90b00e1745177bf12b0fccb03a18b406
Tarik <49348194+tarizer@users.noreply.github.com>
2021-01-08 21:47:47 +0100

    remove unnecessary while

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,29 +1,22 @@
-#include <stdio.h>
-#include <math.h>
-#include <cs50.h>
-
 int main(void)
 {
-    int coins;
-    int cents;
-    float dollars;
+  int coins;
+  int cents;
+  float dollars;
 
-    while ((dollars = get_float("Change owed: ")) < 0);
-    cents = round(dollars * 100);
-    coins = 0;
-    // Get number of coins for the given change
-    // 4 types of coins: 25, 10, 5, 1
-    while (cents > 0)
-    {
-        coins = cents / 25;
-        cents = cents % 25;
-        coins += cents / 10;
-        cents = cents % 10;
-        coins += cents / 5;
-        cents = cents % 5;
-        coins += cents / 1;
-        cents = cents % 1;
-    }
-    // printf("Cents remaining: %i\n", cents);
-    printf("%i\n", coins);
+  while ((dollars = get_float("Change owed: ")) < 0)
+    ;
+  cents = round(dollars * 100);
+  coins = 0;
+  // Get number of coins for the given change -> 4 types of coins: 25, 10, 5, 1
+  coins = cents / 25;
+  cents = cents % 25;
+  coins += cents / 10;
+  cents = cents % 10;
+  coins += cents / 5;
+  cents = cents % 5;
+  coins += cents / 1;
+  cents = cents % 1;
+  // printf("Cents remaining: %i\n", cents);
+  printf("%i\n", coins);
 }
