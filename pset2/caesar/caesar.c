7c14344057aa9038ac0d6522a2b868990ba63672
Tarik <49348194+tarizer@users.noreply.github.com>
2021-01-10 22:55:28 +0100

    Refactor caesar.c

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -3,46 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-// #include <math.h>
 
 int check_args(int argc, char *argv[]);
+string get_ciphertext(string plaintext, int key);
 
 int main(int argc, char *argv[])
 {
-  int length; // For plaintext
-  int key;
   string plaintext;
   string ciphertext;
 
-  // Check for input errors
-  if (check_args(argc, argv) == 1)
+  if (check_args(argc, argv) == 1) // Check for input errors
   {
     printf("Usage: ./caesar key\n");
     return (1);
   }
-  key = atoi(argv[1]);
-  // Prompt user for for plain text
-  plaintext = get_string("plaintext: ");
-  // printf("So far so good!\n");
-  // printf("Key is: %s\n", argv[1]);
-  //
-  // Cipher the text
-  length = strlen(plaintext);
-  ciphertext = plaintext; // So that we can iterate through it and focus only on ciphering alphabetical characters
-  while (length-- > 0)
-  {
-    if (isupper(plaintext[length]))
-    {
-      // Give the character a value between 0 and 25 to be able to use modulus then add it back
-      ciphertext[length] = (plaintext[length] - 65 + key) % 26 + 65;
-    }
-    else if (islower(plaintext[length]))
-    {
-      ciphertext[length] = (plaintext[length] - 97 + key) % 26 + 97;
-    }
-  }
-  // Outout ciphertext
-  printf("ciphertext: %s\n", ciphertext);
+  plaintext = get_string("plaintext: ");                 // Prompt user for for plain text
+  ciphertext = get_ciphertext(plaintext, atoi(argv[1])); // Get ciphertext
+  printf("ciphertext: %s\n", ciphertext);                // Outout ciphertext
 }
 
 int check_args(int argc, char *argv[])
@@ -65,4 +42,29 @@ int check_args(int argc, char *argv[])
     return (1);
   }
   return (0);
+}
+
+string get_ciphertext(string plaintext, int key)
+{
+  int length;
+  string ciphertext;
+
+  // printf("So far so good!\n");
+  // printf("Key is: %s\n", argv[1]);
+  // Cipher the text
+  length = strlen(plaintext);
+  ciphertext = plaintext; // So that we can iterate through it and focus only on ciphering alphabetical characters
+  while (length-- > 0)
+  {
+    if (isupper(plaintext[length]))
+    {
+      // Give the character a value between 0 and 25 to be able to use modulus then add it back
+      ciphertext[length] = (plaintext[length] - 65 + key) % 26 + 65;
+    }
+    else if (islower(plaintext[length]))
+    {
+      ciphertext[length] = (plaintext[length] - 97 + key) % 26 + 97;
+    }
+  }
+  return ciphertext;
 }
